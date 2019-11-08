32a94e72eb73fd375b93cde17532d2b9e26b1254
Conde <mxf942@my.utsa.edu>
2019-11-07 20:13:50 -0600

    init commit

---
+++ CS4743-Assignment4/src/command/ElevatorDirection.java
@@ -0,0 +1,5 @@
+package command;
+
+public enum ElevatorDirection {
+    ELEVATOR_UP, ELEVATOR_DOWN, ELEVATOR_HOLD;
+}
---
+++ CS4743-Assignment4/src/launcher/launcher.java
@@ -0,0 +1,9 @@
+package launcher;
+
+public class launcher {
+    //elevator.press2();
+    //elevator.press1();
+    //elevator.press3();
+    //elevator.press3();
+    //elevator.press1();
+}
