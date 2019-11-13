5593e9b0bedb992986f726567917f57c5f771ff6
Conde <mxf942@my.utsa.edu>
2019-11-12 20:29:19 -0600

    uncomment main

--- CS4743-Assignment4/src/launcher/launcher.java
+++ CS4743-Assignment4/src/launcher/launcher.java
@@ -2,13 +2,13 @@ package launcher;
 
 public class launcher {
 	public static void main(String[] args) {
-		//Elevator elevator = new Elevator();
+		Elevator elevator = new Elevator();
 		
-	    //elevator.press2();
-	    //elevator.press1();
-	    //elevator.press3();
-	    //elevator.press3();
-	    //elevator.press1();
+	    	elevator.press2();
+	    	elevator.press1();
+	    	elevator.press3();
+	    	elevator.press3();
+	    	elevator.press1();
 		
 	}
 
